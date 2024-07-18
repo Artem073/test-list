ad058ffeee961863f6894d05c1426624c6b2d6f0
Артем Харламов <xarlamov_2000@list.ru>
2024-07-18 12:52:07 +0400

    Фикс.

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/tasks', [TaskController::class, 'edit'])->name('tasks.edit');
+    Route::get('/task', [TaskController::class, 'edit'])->name('tasks.edit');
     Route::post('/task', [TaskController::class, 'store'])->name('tasks.create');
     Route::delete('/task/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
 });
